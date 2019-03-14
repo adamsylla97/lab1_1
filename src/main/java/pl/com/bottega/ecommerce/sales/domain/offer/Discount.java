42d6c7a41d85804f0ce25212effec281fdd64e1f
adamsylla97 <209474@edu.p.lodz.pl>
2019-03-14 18:22:56 +0100

    *added constructor, getters and setters to Discount *moved variables used in Discount from OfferItem to Discount class *removed unused variables from OfferItem

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Discount.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Discount.java
@@ -1,5 +1,30 @@
 package pl.com.bottega.ecommerce.sales.domain.offer;
 
+import java.math.BigDecimal;
+
 public class Discount {
 
+    private String cause;
+    private Money ammount;
+
+    public Discount(String cause, Money ammount) {
+        this.cause = cause;
+        this.ammount = ammount;
+    }
+
+    public String getCause() {
+        return cause;
+    }
+
+    public Money getAmmount() {
+        return ammount;
+    }
+
+    public void setCause(String cause) {
+        this.cause = cause;
+    }
+
+    public void setAmmount(Money ammount) {
+        this.ammount = ammount;
+    }
 }
